592dfd7ca15060729c59d1eda3893ec314bb1d0d
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-08-08 15:53:37 +0700

    forum list with ListView

--- forums/views.py
+++ forums/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.shortcuts import render,get_object_or_404
+from django.views.generic import CreateView, ListView
 from .models import Forum
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+
+class ForumListView(ListView):
+	model = Forum
+	#context_object_name = 'forums'
+
+class ForumUserListView(ListView):
+	def get_queryset(self):
+		self.user = get_object_or_404(User, username=self.kwargs['username'])
+		return Forum.objects.filter(user = self.user)
 
 @method_decorator(login_required, name='dispatch')
 
