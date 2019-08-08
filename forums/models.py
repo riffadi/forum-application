de6416f4abdeaa34dbc039ab8ebcd9b672f9baec
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-08-08 10:05:07 +0700

    add slug field

--- forums/models.py
+++ forums/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Forum(models.Model):
@@ -7,6 +8,11 @@ class Forum(models.Model):
 	title = models.CharField(max_length=100)
 	desc = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Forum, self).save(*args, **kwargs)
+
 
 class Comment(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
